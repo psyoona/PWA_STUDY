530a0475993cfcc100b666bf9d6b20122120cce9
Seyoon Park <psyoona@naver.com>
2023-01-04 01:11:40 +0900

    vue project 예제 추가

---
+++ ex05-07/src/main_page.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+	<div>
+		<h2>메인 페이지입니다.</h2>
+		<button @click="moveToSubPage">서브 페이지로 이동(라우터 함수 이용)</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			moveToSubPage(){
+				this.$router.push('/sub');
+			}
+		}
+	}
+</script>
---
+++ ex05-07/src/sub_page.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+	<div>
+		<h2>서브 페이지입니다.</h2>
+		<button @click="moveToMainPage">메인 페이지로 이동(라우터 함수 이용)</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			moveToMainPage(){
+				this.$router.push('/main');
+			}
+		}
+	}
+</script>
